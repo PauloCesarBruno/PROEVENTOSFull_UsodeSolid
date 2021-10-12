0817838f3a347db35f0a51330b12d03dc262190c
Paulo Cesar C Bruno <p_bruno001@hotmail.com>
2021-10-12 15:00:38 -0300

    FullStack Seção_2: Net5 AddMigrations

--- Back/src/ProEventos.API/Controllers/EventoController.cs
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using ProEventos.API.Data;
 using ProEventos.API.models;
 // ====================================
 
@@ -10,43 +11,24 @@ namespace ProEventos.API.Controllers
     [ApiController]
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
-    {
-        public IEnumerable<Evento> _evento =  new Evento[] {
-                new Evento(){
-                EventoId = 1,
-                Tema = "Angular 11 e .NET 5",
-                Local = "Belo Horizonte",
-                Lote = "1º Lote",
-                QtdPessoas = 250,
-                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                ImagemURL = "foto.png"
-                },
+    {       
+        private readonly DataContext _context;
 
-                new Evento(){
-                EventoId = 2,
-                Tema = "Angular e suas novidade",
-                Local = "São Paulo",
-                Lote = "2º Lote",
-                QtdPessoas = 350,
-                DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
-                ImagemURL = "foto1.png"
-                }
-            };   
-
-        public EventoController()
-        {            
+        public EventoController(DataContext context)
+        {
+            _context = context;
         }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _evento; 
+            return _context.Eventos;
         }
 
         [HttpGet("{Id}")]
-        public IEnumerable<Evento> GetById(int Id)
+        public Evento GetById(int Id)
         {
-            return _evento.Where(e=> e.EventoId == Id);
+            return _context.Eventos.FirstOrDefault(e => e.EventoId == Id);
         }
 
         [HttpPost]
